3f0003ed535b0676f688b3877f346ea0ceb589bf
Dawson <37384968+DeeJayNZ@users.noreply.github.com>
2019-03-01 12:34:11 +1300

    Delete untitled-1.py

--- untitled-1.py
+++
@@ -1,6 +0,0 @@
-tickets = [
-    Ticket(PLACEHOLDER, "superman", "superman@superman.com", "15.12.1987", False),
-    Ticket(PLACEHOLDER, "batman", "batman@batman.com", "23.07.1989", False),
-    Ticket(PLACEHOLDER, "hulk", "hulk@brucebanner.com", "26.06.2003", False),
-    Ticket(PLACEHOLDER, "robin", "robin@somepoororphan.com", "25.09.2000", False)
-    ]
